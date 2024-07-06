97ea9342a27dcf9b3456b514f56f66c364618f6b
ivan <ivan@MSI>
2024-07-05 18:56:30 -0700

    Added clock to LED display

--- test/led_matrix/led_matrix.ino
+++ test/led_matrix/led_matrix.ino
@@ -1,12 +1,15 @@
 #include "SPI.h"
 #include "font.h"
 #include "lcd8x8rgb.h"
+#include "clock.h"
 
 unsigned long startTime;  // to store the start time
 char timeString[5]; 
+myClock thisClock;
 
 void setup() {
    Serial.begin(9600);
+  thisClock.clockInitialize();
 
    SPI.setDataMode(SPI_MODE1);
    SPI.setClockDivider(SPI_CLOCK_DIV64);
@@ -17,8 +20,27 @@ void setup() {
 }
 
 void loop() {
-  unsigned long currentTime = millis();
-  unsigned long elapsedMillis = currentTime - startTime;
+
+  fullTime currentTime = thisClock.currentTime();
+  fullDate currentDate = thisClock.currentDate();
+  
+    Serial.print("ESP32 RTC Date Time: ");
+  Serial.print(currentDate.year + 2000); // Adjust year back to full year format
+  Serial.print('/');
+  Serial.print(currentDate.month);
+  Serial.print('/');
+  Serial.print(currentDate.day);
+  Serial.print(" (");
+  Serial.print(thisClock.currentDay());
+  Serial.print(") ");
+  Serial.print(currentTime.hour);
+  Serial.print(':');
+  Serial.print(currentTime.minute);
+  Serial.print(':');
+  Serial.println(currentTime.second);
+
+  unsigned long currTime = millis();
+  unsigned long elapsedMillis = currTime - startTime;
 
   // Calculate time variables
   unsigned long elapsedSeconds = elapsedMillis / 1000;
@@ -30,10 +52,10 @@ void loop() {
   cY = 1;
 
   // Convert numbers to characters
-  timeString[0] = '0' + (minutes / 10);  
-  timeString[1] = '0' + (minutes % 10);  
-  timeString[2] = '0' + (seconds / 10);  
-  timeString[3] = '0' + (seconds % 10);  
+  timeString[0] = '0' + (currentTime.minute/ 10);  
+  timeString[1] = '0' + (currentTime.minute % 10);  
+  timeString[2] = '0' + (currentTime.second / 10);  
+  timeString[3] = '0' + (currentTime.second % 10);  
 
   // Draw string on LED matrix
   drawString(timeString);
