d517a4a5ff1d630ba66ca719ba3810cb98207375
william hernandez <william.hernandez@sjsu.edu>
2024-07-05 21:31:02 -0700

    finished alarm

--- smart_clock/smart_clock.ino
+++ smart_clock/smart_clock.ino
@@ -1,85 +1,4 @@
-// 
-//#include "clock.h"
-//#include "uart_player.h"
-//
-//uart_player musicPlayer;
-//myClock thisClock;
-//
-//bool alarmStatus = false;
-//bool buttonStatus = false;
-//
-//void buttonInterrupt() { //up
-//  buttonStatus = true;
-//}
-//
-//void alarm(int setHour, int setMinute, bool status){
-//  fullTime currentTime = thisClock.currentTime();  // Move these inside the function
-////  Serial.print(currentTime.hour);
-////  Serial.print(" ");
-////  Serial.println(setHour);
-////  Serial.print(currentTime.minute );
-////  Serial.print( " " );
-////  Serial.println( setMinute);
-//  if(setHour == currentTime.hour && setMinute == currentTime.minute && status == true){ 
-//    alarmStatus = true;
-//  }
-//
-//  if(alarmStatus == false){
-//    Serial.println( "turning of the alarm");
-//    musicPlayer.music_player__stop();
-//  }else{
-//    musicPlayer.music_player__volume(255);
-//    musicPlayer.music_player__song(2);
-//    alarmStatus = true;
-//  }
-//}
-//
-//void setup() {
-//  Serial.begin(9600); 
-//  thisClock.clockInitialize();
-//  pinMode(2, INPUT_PULLUP); // Enable internal pull-up resistor
-//  attachInterrupt(digitalPinToInterrupt(2), buttonInterrupt, FALLING);
-//  delay(1000);
-//}
-//
-//void loop() { 
-//  fullTime currentTime = thisClock.currentTime();  // Move these inside the function
-//  fullDate currentDate = thisClock.currentDate();  // Move these inside the function
-//  int min = currentTime.minute;
-//  int hou = currentTime.hour;
-//  int sec = currentTime.second;
-//  int today = currentDate.day;
-//  int mon = currentDate.month;
-//  int yea = currentDate.year; 
-//  
-//  Serial.print("ESP32 RTC Date Time: ");
-//  Serial.print(mon);
-//  Serial.print('/');
-//  Serial.print(today);
-//  Serial.print('/');
-//  Serial.print(yea);
-//  Serial.print(" (");
-//  Serial.print(thisClock.currentDay());
-//  Serial.print(") ");
-//  Serial.print(hou);
-//  Serial.print(':');
-//  Serial.print(min);
-//  Serial.print(':');
-//  Serial.println(sec);
-//
-//  
-//  if(buttonStatus == true){
-//    
-//    alarmStatus = false;  //  
-//    buttonStatus = false;
-//  } else {
-//     
-//    alarm(14, 0, true); 
-//     
-//  }
-//
-//   delay(3000);
-//}
+
 #include "clock.h"
 #include "uart_player.h"
 
@@ -93,16 +12,30 @@ void buttonInterrupt() { //up
   buttonStatus = true;
 }
 
-void alarm(int setHour, int setMinute, bool status) {
-  fullTime currentTime = thisClock.currentTime();
-  if (setHour == currentTime.hour && setMinute == currentTime.minute && status == true) {
-    musicPlayer.music_player__song(2);
-    alarmStatus = true;
-  }
+bool stopAlarm = false;
+bool stopMusic = true;
 
-  if (alarmStatus == false) {
-    musicPlayer.music_player__stop();
+void alarm(int setHour, int setMinute, bool setAlarm) {
+  fullTime currentTime = thisClock.currentTime();
+  
+  if(setAlarm){
+    if (setHour == currentTime.hour && setMinute == currentTime.minute && !stopAlarm) {
+      musicPlayer.music_player__song(2);
+      stopAlarm = true; 
+      stopMusic = false;
+    }
+    
+    if(!stopMusic){
+      musicPlayer.music_player__song(2);
+    }else{
+      if(setHour != currentTime.hour && setMinute != currentTime.minute){
+        stopAlarm = false;  
+      }
+      
+      musicPlayer.music_player__stop();
+    }
   }
+ 
 }
 
 void setup() {
@@ -115,16 +48,15 @@ void setup() {
 
 void loop() { 
   if (buttonStatus == true) {
-    alarmStatus = false;  
+    stopMusic = true;  
     buttonStatus = false;
   } else {
-    alarm(3, 0, true); 
-    alarmStatus = false;
+    alarm(21, 22, true);  
   }
 
   fullTime currentTime = thisClock.currentTime();
   fullDate currentDate = thisClock.currentDate();
-  
+  //for debuging process can erased later
   Serial.print("ESP32 RTC Date Time: ");
   Serial.print(currentDate.year + 2000); // Adjust year back to full year format
   Serial.print('/');
--- test/music_player/uart_player.cpp
+++ test/music_player/uart_player.cpp
@@ -1,11 +1,9 @@
-#include "uart_player.h"
-#include <stdint.h>
-#include <stdio.h>
+#include "uart_player.h" 
 
  
 HardwareSerial myUart(1);
-uart_player::uart_player()
 
+uart_player::uart_player() 
 {
   // Initialize serial and wait for port to open:
   Serial.begin(9600);
@@ -14,48 +12,47 @@ uart_player::uart_player()
 }
 
 uint8_t uart_player::music_player__checksum(uint8_t *uart_commad, uint8_t length) {
-  uint8_t sum = 0;
-  for (uint8_t i = 0; i < length; i++) {
-    sum = sum + uart_commad[i];
-  }
-  return sum;
+    uint8_t sum = 0;
+    for (uint8_t i = 0; i < length; i++) {
+        sum = sum + uart_commad[i];
+    }
+    return sum;
 }
 
 void uart_player::music_player__uart__polled_put(uint8_t *uart_commad, uint8_t length) {
-  uint8_t checksum = music_player__checksum(uart_commad, length);
-  Serial.println(checksum); 
-  myUart.write(uart_commad, length);   
-  myUart.write(&checksum, 1);
+    uint8_t checksum = music_player__checksum(uart_commad, length); 
+    myUart.write(uart_commad, length);   
+    myUart.write(&checksum, 1);
 }
 
 void uart_player::music_player__loop_mode(uint8_t mode){
-  uint8_t uart_commad[4] = {0xaa, 0x18, 0x01, 0x00};
-  uart_commad[3] = mode; 
-  music_player__uart__polled_put(uart_commad, 4);
+    uint8_t uart_commad[4] = {0xaa, 0x18, 0x01, 0x00};
+    uart_commad[3] = mode; 
+    music_player__uart__polled_put(uart_commad, 4);
 }
 
 void uart_player::music_player__play(void){
-  uint8_t uart_commad[4] = {0xaa, 0x02, 0x00, 0xac};
+    uint8_t uart_commad[4] = {0xaa, 0x02, 0x00, 0xac};
 
-  for (int i = 0; i < 4; i++) { 
-    myUart.write(uart_commad, sizeof(uart_commad));
-  }
+    for (int i = 0; i < 4; i++) { 
+        myUart.write(uart_commad, sizeof(uart_commad));
+    }
 }
 void uart_player::music_player__song(uint16_t number){
-  uint8_t uart_commad[5] = {0xaa, 0x07, 0x02, 0x00, 0x00}; 
-  uart_commad[4] = number & 0xFF;
+    uint8_t uart_commad[5] = {0xaa, 0x07, 0x02, 0x00, 0x00}; 
+    uart_commad[4] = number & 0xFF;
 
-  music_player__uart__polled_put(uart_commad, 5); 
+    music_player__uart__polled_put(uart_commad, 5); 
 } 
 
 void uart_player::music_player__stop(void){
-  uint8_t uart_commad[4] = {0xaa, 0x04, 0x00, 0xae};
+    uint8_t uart_commad[4] = {0xaa, 0x04, 0x00, 0xae};
 
-  music_player__uart__polled_put(uart_commad, 4);
+    music_player__uart__polled_put(uart_commad, 4);
 }
 void uart_player::music_player__volume(uint16_t volume){
-  uint8_t uart_commad[4] = {0xaa, 0x13, 0x01, 0x00};
-  uart_commad[3] = volume;
+    uint8_t uart_commad[4] = {0xaa, 0x13, 0x01, 0x00};
+    uart_commad[3] = volume;
 
-  music_player__uart__polled_put(uart_commad, 4);
+    music_player__uart__polled_put(uart_commad, 4);
 }
